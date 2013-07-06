85af1a5438645c55bbb5a8015208e688b1c6c254
Dominik Marszk <dmarszk@gmail.com>
2013-07-06 17:50:16 +0100

    [TZ] Added full TZSW for GC100.

--- sd_fuse/create_4412_sd_image.sh
+++ sd_fuse/create_4412_sd_image.sh
@@ -47,7 +47,8 @@ echo "Exynos4412 bootloader fusing"
 dd iflag=dsync oflag=dsync if=../u-boot.bin of=$1 seek=$uboot_pos
 ####################################
 echo "Exynos4412 tzsw fusing"
-dd iflag=dsync oflag=dsync if=./gcam_tzsw.bin of=$1 seek=$tzsw_pos
+dd iflag=dsync oflag=dsync if=./gcam_sdcard_tzsw.bin of=$1 
+seek=$tzsw_pos
 ####################################
 echo "Filling env with zeroes.."
 dd iflag=dsync oflag=dsync if=/dev/zero of=$1 bs=512 count=$env_size seek=$env_pos
